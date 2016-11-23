ed625ec111e45cec571deaba7e8703b7e0f1ce90
kolobash <kolobov_v@ukr.net>
2016-11-23 13:25:08 +0200

    Обновил скрипт

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 var write = document.querySelector(".write-us");
 var modal = document.querySelector(".modal-content");
 var modalClose = document.querySelector(".modal-content-close");
-var form = modal.querySelector("form");
-var userName = modal.querySelector("[name=user-name]");
-var email = modal.querySelector("[name=email]");
-var comment = modal.querySelector("textarea");
 var buy = document.querySelectorAll(".buy");
-var popup = document.querySelectorAll(".pop-up");
+var popup = document.querySelector(".pop-up");
 var popClose = document.querySelectorAll(".pop-up-close");
+var map = document.querySelector(".modal-map")
+var mapUp = document.querySelector(".map")
+var mapClose = document.querySelector(".map-close")
 
-if (buy){
-	for (var i=0; i<buy.length; i++) {
+if (buy) {
+	for (var i=0; i < buy.length; i++) {
 		buy[i].addEventListener("click", function(event) {
 			event.preventDefault();
 			popup.classList.add("pop-up-show");
 		});
-	}	
+	}
 	for (var i=0; i<popClose.length; i++) {
 		popClose[i].addEventListener("click", function(event) {
 			event.preventDefault();
@@ -31,15 +30,17 @@ if (buy){
 		}
 	})
 }
-
-if (write && modal) {
+if (modal) {
+	var userName = modal.querySelector("[name=user-name]");
+	var email = modal.querySelector("[name=email]");
+	var comment = modal.querySelector("textarea");
 	write.addEventListener("click", function(event){
 		event.preventDefault();
 		modal.classList.add("modal-content-show");
 		userName.focus();
 	});
 
-	form.addEventListener("submit", function(event) {
+	modal.addEventListener("submit", function(event) {
 		if (!userName.value || !email.value || !comment){
 			event.preventDefault();
 			console.log("Заполните все поля формы!");
@@ -60,3 +61,20 @@ if (write && modal) {
 	});
 }
 
+map.addEventListener("click", function(event){
+		event.preventDefault();
+		mapUp.classList.add("map-show");
+	});
+
+mapClose.addEventListener("click", function(event){
+		event.preventDefault();
+		mapUp.classList.remove("map-show");
+	});
+
+window.addEventListener("keydown", function() {
+		if (event.keyCode === 27) {
+			if (mapUp.classList.contains("map-show")) {
+				mapUp.classList.remove("map-show");
+			}
+		}
+	});
