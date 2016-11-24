952a09cefe77cd4143baae222047fff80eccdbf2
kolobash <kolobov_v@ukr.net>
2016-11-24 15:36:49 +0200

    Внес изменения

--- js/script.js
+++ js/script.js
@@ -9,72 +9,72 @@ var mapUp = document.querySelector(".map")
 var mapClose = document.querySelector(".map-close")
 
 if (buy) {
-	for (var i=0; i < buy.length; i++) {
-		buy[i].addEventListener("click", function(event) {
-			event.preventDefault();
-			popup.classList.add("pop-up-show");
-		});
-	}
-	for (var i=0; i<popClose.length; i++) {
-		popClose[i].addEventListener("click", function(event) {
-			event.preventDefault();
-			popup.classList.remove("pop-up-show");
-		});
-	}
+    for (var i = 0; i < buy.length; i++) {
+        buy[i].addEventListener("click", function(event) {
+            event.preventDefault();
+            popup.classList.add("pop-up-show");
+        });
+    }
+    for (var i = 0; i < popClose.length; i++) {
+        popClose[i].addEventListener("click", function(event) {
+            event.preventDefault();
+            popup.classList.remove("pop-up-show");
+        });
+    }
 
-	window.addEventListener("keydown", function() {
-		if (event.keyCode === 27) {
-			if (popup.classList.contains("pop-up-show")) {
-				popup.classList.remove("pop-up-show");
-			}
-		}
-	})
+    window.addEventListener("keydown", function() {
+        if (event.keyCode === 27) {
+            if (popup.classList.contains("pop-up-show")) {
+                popup.classList.remove("pop-up-show");
+            }
+        }
+    })
 }
 if (modal) {
-	var userName = modal.querySelector("[name=user-name]");
-	var email = modal.querySelector("[name=email]");
-	var comment = modal.querySelector("textarea");
-	write.addEventListener("click", function(event){
-		event.preventDefault();
-		modal.classList.add("modal-content-show");
-		userName.focus();
-	});
+    var userName = modal.querySelector("[name=user-name]");
+    var email = modal.querySelector("[name=email]");
+    var comment = modal.querySelector("textarea");
+    write.addEventListener("click", function(event) {
+        event.preventDefault();
+        modal.classList.add("modal-content-show");
+        userName.focus();
+    });
 
-	modal.addEventListener("submit", function(event) {
-		if (!userName.value || !email.value || !comment){
-			event.preventDefault();
-			console.log("Заполните все поля формы!");
-		}
-	});
+    modal.addEventListener("submit", function(event) {
+        if (!userName.value || !email.value || !comment) {
+            event.preventDefault();
+            console.log("Заполните все поля формы!");
+        }
+    });
 
-	modalClose.addEventListener("click", function(event){
-		event.preventDefault();
-		modal.classList.remove("modal-content-show");
-	});
+    modalClose.addEventListener("click", function(event) {
+        event.preventDefault();
+        modal.classList.remove("modal-content-show");
+    });
 
-	window.addEventListener("keydown", function() {
-		if (event.keyCode === 27) {
-			if (modal.classList.contains("modal-content-show")) {
-				modal.classList.remove("modal-content-show");
-			}
-		}
-	});
+    window.addEventListener("keydown", function() {
+        if (event.keyCode === 27) {
+            if (modal.classList.contains("modal-content-show")) {
+                modal.classList.remove("modal-content-show");
+            }
+        }
+    });
 }
 
-map.addEventListener("click", function(event){
-		event.preventDefault();
-		mapUp.classList.add("map-show");
-	});
+map.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapUp.classList.add("map-show");
+});
 
-mapClose.addEventListener("click", function(event){
-		event.preventDefault();
-		mapUp.classList.remove("map-show");
-	});
+mapClose.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapUp.classList.remove("map-show");
+});
 
 window.addEventListener("keydown", function() {
-		if (event.keyCode === 27) {
-			if (mapUp.classList.contains("map-show")) {
-				mapUp.classList.remove("map-show");
-			}
-		}
-	});
+    if (event.keyCode === 27) {
+        if (mapUp.classList.contains("map-show")) {
+            mapUp.classList.remove("map-show");
+        }
+    }
+});
